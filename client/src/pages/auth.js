7ac421d3ee8bc19be7d00db3d26bc9bda511b14c
Chidananda Sahu <chidananda@sugarcosmetics.com>
2023-03-09 18:43:39 +0530

    add authenticatoin

--- client/src/pages/auth.js
+++ client/src/pages/auth.js
@@ -1,12 +1,96 @@
-
+import { useState } from "react";
+import axios from "axios"
+import {useCookies} from "react-cookie"
+import {useNavigate} from "react-router-dom"
 export const Auth = () => {
-    return <div>
-        auth
+  return (
+    <div>
+      <Login />
+      <Register />
     </div>
-}
+  );
+};
 
-export const Login = () => {
-    return <div>
-        Login
+export const Form = ({ username, password, setUserName, setPassword, label, onSubmit }) => {
+  return (
+    <div className="auth-container">
+      <form onSubmit={onSubmit}>
+        <h2>{label}</h2>
+        <div className="form-group">
+          <label htmlFor="username">Username : </label>
+          <input
+            type="text"
+            id="username"
+            value={username}
+            onChange={(event) => setUserName(event.target.value)}
+          />
+        </div>
+        <div className="form-group">
+          <label htmlFor="password">Password : </label>
+          <input
+            type="password"
+            id="password"
+            value={password}
+            onChange={(event) => setPassword(event.target.value)}
+          />
+        </div>
+        <button type="submit">{label}</button>
+      </form>
     </div>
-}
+  );
+};
+
+const Register = () => {
+  const [username, setUserName] = useState("");
+  const [password, setPassword] = useState("");
+
+  const onSubmit = async (event) => {
+    event.preventDefault();
+    try {
+        await axios.post("http://localhost:3001/api/auth/register", {username, password})
+        alert("Registration Completed! Now Login.")
+    } catch (error) {
+        console.error(error)
+    }
+  }
+  return (
+    <Form
+      username={username}
+      setUserName={setUserName}
+      password={password}
+      setPassword={setPassword}
+      label="Register"
+      onSubmit={onSubmit}
+    />
+  );
+};
+
+const Login = () => {
+  const [username, setUserName] = useState("");
+  const [password, setPassword] = useState("");
+  const [_, setCookies] = useCookies(["access_token"])
+  const navigate = useNavigate()
+
+  const onSubmit = async (event) => {
+    event.preventDefault();
+    try {
+        const response = await axios.post("http://localhost:3001/api/auth/login", {username, password})
+        setCookies("access_token", response.data.token)
+        window.localStorage.setItem("userID", response?.data?.userID)
+        navigate("/")
+        
+    } catch (error) {
+        
+    }
+  }
+  return (
+    <Form
+      username={username}
+      setUserName={setUserName}
+      password={password}
+      setPassword={setPassword}
+      label="Login"
+      onSubmit={onSubmit}
+    />
+  );
+};
